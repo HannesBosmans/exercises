b34c3ab0dcf2610fcf9b3604d8cb583d6101cbe2
Hannes Bosmans <hannes.bosmans@student.ucll.be>
2024-02-20 15:49:54 +0100

    inheritence

--- 02-oo/01-classes/06-objects/student.py
+++ 02-oo/01-classes/06-objects/student.py
@@ -1,6 +1,11 @@
 def main():
     # write your code here
-
+ Aragorn = Brawler(4,4,"Aragorn")
+ gimli = Brawler(2,7,"Gimli")
+ legolas = Brawler(2,7,"Legolas")
+ frodo = Brawler(3,2,"Frodo")
+ fight(Aragorn,gimli)
+ fight(legolas,frodo)
 
 # don't touch below this line
 
@@ -11,7 +16,6 @@ class Brawler:
         self.strength = strength
         self.power = speed * strength
         self.name = name
-
 
 def fight(f1, f2):
     if f1.power > f2.power:
--- 02-oo/03-properties/02-computed-attribute/student.py
+++ 02-oo/03-properties/02-computed-attribute/student.py
@@ -1,1 +1,26 @@
 # Write your code here
+class BMICalculator:
+    def __init__(self,weight_in_kg,height_in_m):
+        self.__weight_in_kg = weight_in_kg
+        self.__heigth_in_m = height_in_m
+
+    @property
+    def weigth_in_kg(self):
+        return self.__weight_in_kg
+    @property
+    def heigth_in_m(self):
+        return self.__heigth_in_m
+    @property
+    def bmi(self):
+        return self.weigth_in_kg / self.heigth_in_m **2
+    @property
+    def category(self):
+        if self.bmi < 18.5:
+            return "underweigth"
+        elif 18.5< self.bmi < 25:
+            return "normal"
+        else: return "overweight"
+
+calc = BMICalculator(80,1.80)
+print(calc.bmi)
+print(calc.category)
